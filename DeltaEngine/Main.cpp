6b0775de540c81a22b8813262f440849b21f87a7
Jackly Ying <jwm.ying@student.avans.nl>
2024-09-12 10:01:36 +0200

    Faulty Code

--- DeltaEngine/Main.cpp
+++ DeltaEngine/Main.cpp
@@ -2,5 +2,11 @@
 
 int main()
 {
+	if (true)
+	{          // Space after the keyword in conditions and loops.
+	}
+	else
+	{          // Spaces around else.
+	}
 	std::cout << "Meow!" << std::endl;
 }
