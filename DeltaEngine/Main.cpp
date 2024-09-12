b35b27919164adab5ad054288f74f0c286339db2
Jackly Ying <jwm.ying@student.avans.nl>
2024-09-12 10:07:55 +0200

    Update Main.cpp

--- DeltaEngine/Main.cpp
+++ DeltaEngine/Main.cpp
@@ -2,12 +2,5 @@
 
 int main()
 {
-	if (true)
-	{          // Space after the keyword in conditions and loops.
-	}
-	else
-	{          // Spaces around else.
-	}
-	int _vieze1variabelNaam = 0;
 	std::cout << "Meow!" << std::endl;
-}
+} 
