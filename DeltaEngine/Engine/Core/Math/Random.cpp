8aacaa05d74956058e64489673bdedf5710f21a5
Jackly Ying <jacklyying@hotmail.com>
2024-11-13 14:59:00 +0100

    Added Mathematical Helper functions for Point, Vector2 and added a Random class as Singleton.

---
+++ DeltaEngine/Engine/Core/Math/Random.cpp
@@ -0,0 +1,41 @@
+#include "Random.hpp"
+
+using namespace Math;
+
+Random Random::instance;
+std::random_device Random::rnd;
+
+int Random::NextInt(int min, int max) const
+{
+	if (min > max)
+	{
+		throw std::invalid_argument("Min should not be greater than max.");
+	}
+
+	std::uniform_int_distribution<int> dist(min, max);
+
+	return dist(rnd);
+}
+
+double Random::NextDouble(int min, int max) const
+{
+	if (min > max)
+	{
+		throw std::invalid_argument("Min should not be greater than max.");
+	}
+
+	std::uniform_real_distribution<double> dist(min, max);
+
+	return dist(rnd);
+}
+
+double Random::NextDouble() const
+{
+	std::uniform_real_distribution<double> dist(0, 1);
+	return dist(rnd);
+}
+
+bool Random::NextBool() const
+{
+	return NextInt(0, 1) == 1;
+}
