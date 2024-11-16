c680130d42e246e592b39f2a9aa34b78d4e554c3
Yurian <Yurian.van.Hattem@gmail.com>
2024-11-16 22:25:39 +0100

    all listeners work

--- ValidationApp/src/Scripts/TempInput.hpp
+++ ValidationApp/src/Scripts/TempInput.hpp
@@ -12,22 +12,57 @@ class TempInput : public BehaviourScript
 	void OnStart() override { 
 		std::cout << "OnStart" << std::endl; 
 
+		/*InputManager::GetInstance().onKeyDown(
+			"W",
+			[](KeyListener& key)
+			{
+				std::cout << "Gaat naar boven: " << key.key
+						  << std::endl;
+			});
+
 		InputManager::GetInstance().onKeyDown(
-			"K",
+			"A",
 			[](KeyListener& key)
 			{
-				std::cout << "Key event: " << key.key
+				std::cout << "Gaat naar rechts: " << key.key
 						  << std::endl;
-			} );  //Simularen dat er een sdl event komt
+			});
+		InputManager::GetInstance().onKeyDown(
+			"D", 
+			[](KeyListener& key)
+			{ std::cout << "Gaat naar onder: " << key.key << std::endl; });
+		InputManager::GetInstance().onKeyDown(
+			"S", [](KeyListener& key)
+			{ std::cout << "Gaat naar links: " << key.key << std::endl; });
 
+		
 		InputManager::GetInstance().onKeyUp(
-			"M",
-			[](KeyListener& key)
-			{
-				std::cout << "key event: " << key.key
-						  << std::endl;
-			});	 //Simularen dat er een sdl event komt
+			"Space", [](KeyListener& key)
+			{ std::cout << "Spatie is los gelaten: " << key.key << std::endl; });
 
+		InputManager::GetInstance().onMouseButtonDown(
+			1,
+			[](MouseListener& button) {
+				std::cout << "linkermuis click: " << button.button << std::endl;
+			});
+
+		InputManager::GetInstance().onMouseButtonUp(
+			2,
+			[](MouseListener& button) {
+				std::cout << "weel los gelaten: " << button.button << std::endl;
+			});
+
+		InputManager::GetInstance().onMouseMove(
+			[](MouseListener& mouse) {
+				std::cout << mouse.mouseX << " - " << mouse.mouseY << std::endl;
+			});*/
+
+		InputManager::GetInstance().onMouseWheel(
+			[](WheelListener& mouse) {
+				std::cout << mouse.h << " - " << mouse.v << " : "
+						  << mouse.preciseX << " - " << mouse.preciseY << ":"
+						  << mouse.direction << std::endl;
+			});
 	}
 
 	void OnUpdate() override {
