bee27355f5fd6c5a7c4914af3f44bfc787760fcf
JrnJ <jeroenjochems@outlook.com>
2024-11-26 13:45:21 +0100

    quick fixes

--- DeltaEngine/Engine/Core/Math/Vector2.cpp
+++ DeltaEngine/Engine/Core/Math/Vector2.cpp
@@ -98,6 +98,11 @@ Vector2 Vector2::operator-(const Vector2& other) const
 	return Vector2(_x - other._x, _y - other._y);
 }
 
+Vector2 Math::Vector2::operator-() const
+{
+	return Vector2(-_x, -_y);
+}
+
 Vector2 Vector2::operator*(float scalar) const
 {
 	return Vector2(_x * scalar, _y * scalar);
--- ValidationApp/src/Scripts/PlayerBehaviour.cpp
+++ ValidationApp/src/Scripts/PlayerBehaviour.cpp
@@ -4,6 +4,7 @@ void PlayerBehaviour::OnStart()
 {
 	sprite = &gameObject->GetComponent<Sprite>();
 	rigidbody = &gameObject->GetComponent<Rigidbody>();
+	rigidbody->SetGravityScale(0.0f);
 
 	// Events
 	rigidbody->onTriggerExit.Register(
@@ -48,21 +49,21 @@ void PlayerBehaviour::OnUpdate()
 			case FloorType::NORMAL:
 				rigidbody->SetVelocity(_moveDirection * _moveSpeed);
 				break;
+
 			case FloorType::ICE:
 				if (rigidbody->GetSpeed() < _moveSpeed)
 				{
-					rigidbody->AddForce(_moveDirection * _moveSpeed,
-										ForceMode::ACCELERATE);
+					rigidbody->AddForce(_moveDirection * _moveSpeed, ForceMode::ACCELERATE);
 				}
 
 				Math::Vector2 currentVelocity{rigidbody->GetVelocity()};
-				rigidbody->AddForce(-currentVelocity * 1.0f,
-									ForceMode::ACCELERATE);
+				rigidbody->AddForce(-currentVelocity * 1.0f, ForceMode::ACCELERATE);
 				if (rigidbody->GetSpeed() <= 0.0f)
 				{
 					rigidbody->SetVelocity(Math::Vector2(0.0f, 0.0f));
 				}
 				break;
+
 			case FloorType::MUD:
 				rigidbody->SetVelocity(_moveDirection * (_moveSpeed * 0.75f));
 				break;
