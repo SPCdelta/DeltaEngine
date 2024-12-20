14d4b0fd7e67403e95b92eff5c9bcb011fd89615
Jeroen <jeroenjochems@outlook.com>
2024-12-20 12:36:06 +0100

    fix

--- ValidationApp/src/Scenes/DevScene.hpp
+++ ValidationApp/src/Scenes/DevScene.hpp
@@ -8,6 +8,11 @@ class DevScene : public Scene
 public:
 	DevScene(const std::string& name) : Scene{name}
 	{
+
+	}
+
+	void OnStart() override
+	{
 		MenuView menuView{ *this, "Dev Menu", 8, "alucrads", {500, 100}, {200, 50}, 10, 40 };
 		menuView.SetButtonTextColor(-1, {255, 255, 255, 255});
 		menuView.SetButtonText(0, "GameScene");
@@ -15,9 +20,9 @@ public:
 		menuView.SetButtonText(2, "PhysicsScene");
 		menuView.SetButtonText(7, "Back");
 		menuView.AddButtonTextPosition(-1, {20, 5});
-		menuView.SetButtonOnLeftMouseClickLoadScene(0, *this, "GameScene", "Dev Menu");
-		menuView.SetButtonOnLeftMouseClickLoadScene(1, *this, "LayerScene", "Dev Menu");
-		menuView.SetButtonOnLeftMouseClickLoadScene(2, *this, "PhysicsScene", "Dev Menu");
-		menuView.SetButtonOnLeftMouseClickLoadScene(7, *this, "MainMenuScene", "Dev Menu");
+		menuView.SetButtonOnLeftMouseClickLoadScene(0, this, "GameScene", "Dev Menu");
+		menuView.SetButtonOnLeftMouseClickLoadScene(1, this, "LayerScene", "Dev Menu");
+		menuView.SetButtonOnLeftMouseClickLoadScene(2, this, "PhysicsScene", "Dev Menu");
+		menuView.SetButtonOnLeftMouseClickLoadScene(7, this, "MainMenuScene", "Dev Menu");
 	}
 };
