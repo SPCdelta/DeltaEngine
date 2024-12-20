7a03f4557e34fb7d4833a224184a5a0eabe124e1
Yurian <Yurian.van.Hattem@gmail.com>
2024-12-20 15:18:59 +0100

    buttons werken nu ook gewoon normaal

--- DeltaEngine/Engine/UI/Button.hpp
+++ DeltaEngine/Engine/UI/Button.hpp
@@ -1,9 +1,8 @@
 #pragma once
 
-#include "../Core/Math/Vector2.hpp"
-#include "../Core/Math/Point.hpp"
 #include "../Input/DeltaInputs.hpp"
 #include "../Input/InputHandler.hpp"
+#include "../Transform.hpp"
 #include <functional>
 #include <memory>
 #include <vector>
@@ -15,18 +14,14 @@ namespace Ui
 class Button
 {
    public:
-	   // Het actieve gebied waarop je kunt klikken op button is gelijk aan de grootte van de gameObject waaraan die is toegevoegd.
-	Button(const Vector2& position, const Vector2& scale) : _position{position}, _scale{scale} {}
+	Button(Transform* transform) : _transform{ transform }{}
 	void SetOnLeftMouseClick(std::function<void()> func, const std::string& category);
 	void SetOnMouseHover(std::function<void()> func);
 	void SetOnMousePressed(std::function<void()> func, const std::string& category);
-	void SetPosition(const Vector2& scale);
-	void SetScale(const Vector2& scale);
 
    private:
 	InputHandler _inputListeners;
 	bool _pressed = false;
-	Vector2 _position;
-	Vector2 _scale;
+	Transform* _transform;
 };
 }  // namespace Ui
