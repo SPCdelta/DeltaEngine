674a272236c5fdff83628829ee422023b81ec429
Charlotte Klokgieters <charlotteklokgieters@gmail.com>
2025-01-10 12:43:56 +0100

    Reapply "Merge branch 'dev' into feature/player-saving-loading"
    This reverts commit 879987eecb2a2ebc7b184e05a3a986f0e50c14a1.

--- ValidationApp/src/Scenes/LevelEditor.hpp
+++ ValidationApp/src/Scenes/LevelEditor.hpp
@@ -45,8 +45,8 @@ public:
         _screenPort = { {0.0f, topBarHeight}, 0.0f, {static_cast<float>(windowWidth), windowHeight - topBarHeight} };
         
        Json::json data = RetrieveUserData();
-        if (data.contains("fileName") && !data["fileName"].empty()){
-            _saveFileName = data["fileName"];
+        if (data.contains("levelName") && !data["levelName"].empty()){
+            _saveFileName = data["levelName"];
             DeleteUserData();
         }
         InitLevelEditor();
@@ -93,7 +93,7 @@ public:
         }
         FileManager fileManager;
 
-        Json::json loadTiles = fileManager.Load(LEVEL_PATH + _saveFileName + ".json", "json");
+        Json::json loadTiles = fileManager.Load(LEVEL_PATH + _saveFileName, "json");
         
             
         //TODO dit moet eigenlijk een BrushSprite child worden voor elk soort tile type dat anderes gedraagd. hier door kan je het makkelijk opslaan en inladen
@@ -170,11 +170,10 @@ public:
     void MakeSaveFilePath(){
         if (_saveFileName.empty()){
             auto fileNames = FileManager::filesInDirectory(LEVEL_PATH);
-            _saveFileName = "level-" + std::to_string(fileNames.size() + 1);
+            _saveFileName = "level-" + std::to_string(fileNames.size() + 1) + ".json";
         }
         
-        _saveFilePath = LEVEL_PATH + _saveFileName + ".json";
-
+        _saveFilePath = LEVEL_PATH + _saveFileName;
     }
 
 
